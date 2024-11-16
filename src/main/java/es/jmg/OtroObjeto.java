4e5ab204950201bfe785e95965acde15b59aac97
Juanma <Juanma888@gmail.com>
2024-11-16 22:42:30 +0100

    terminación del anterior commit

--- src/main/java/es/jmg/OtroObjeto.java
+++ src/main/java/es/jmg/OtroObjeto.java
@@ -1,7 +1,15 @@
 package es.jmg;
 public class OtroObjeto {
-    // Constructor por defecto (sin parámetros)
-    public OtroObjeto() {
-        System.out.println("¡Se ha creado un objeto de OtroObjeto!");
+// Es instanciada desde otra clase en este mismo directorio: MiPrograma.java
+    private String nombre;
+
+    // Constructor que recibe un nombre
+    public OtroObjeto(String nombre) {
+        this.nombre = nombre; 
+        // this se refiere al declarado en la clase. La parte derecha del =, es el argumento
+    }
+
+    public void saludar() {
+        System.out.println("¡Hola, " + nombre + "!");
     }
 }
