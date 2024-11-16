22023c58ae5222e61a3bcf449b88f1c0f5c85e92
Juanma <Juanma888@gmail.com>
2024-11-16 22:41:26 +0100

    Arregladas la clases MiPrograma y OtroObjeto que están ligadas

---
+++ src/main/java/es/jmg/MiPrograma.java
@@ -0,0 +1,10 @@
+package es.jmg;
+
+public class MiPrograma {
+
+    public static void main(String[] args) {
+        // Crear una instancia de OtroObjeto usando el constructor
+        OtroObjeto obj = new OtroObjeto("Juan");
+        obj.saludar();
+    }
+}
